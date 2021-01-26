625ed70e61ec7de7a86d78b9344beee6b4563923
J.Easter <295140325@qq.com>
2021-01-26 23:36:03 +0800

    class05: 完成作业，实现返回 HTTP GET localhost:8801 的 response body

---
+++ Week_03/class05/netty-proxy/src/main/java/com/xjr/nettyproxy/NettyProxyApplication.java
@@ -0,0 +1,12 @@
+package com.xjr.nettyproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyProxyApplication.class, args);
+    }
+}
---
+++ Week_03/class05/netty-proxy/src/test/java/com/xjr/nettyproxy/NettyProxyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.xjr.nettyproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
